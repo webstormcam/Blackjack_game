814ac23b1ba6f7c94082bce18f25c191323497d6
Cameron Chandler <webstormcam@gmail.com>
2021-05-29 07:59:16 -0400

    Building the essential if else statements

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard +6
+
+if (sum<21){
+    console.log('Do you want to draw a new card?🙂')
+} else if (sum===21){
+    console.log("Woohoo! You've got Blackjack!🥳 ")
+} else {
+    console.log("You're out of the game!😭")
+}
