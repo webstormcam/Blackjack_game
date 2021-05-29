e90441432a6398b608b6903726c692f56d47eadd
Cameron Chandler <webstormcam@gmail.com>
2021-05-29 10:07:13 -0400

    added in some vars
    This tells the game if the player is still alive or has made Blackjack

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard 
+let secondCard = 10
+let sum = firstCard + secondCard +3
+
+let hasBlackJack=false
+let isAlive=true
 
 if (sum<=20){
     console.log('Do you want to draw a new card?🙂')
 } else if (sum===21){
     console.log("Woohoo! You've got Blackjack!🥳 ")
+    hasBlackJack=true
 } else {
     console.log("You're out of the game!😭")
+    isAlive=false
 }
+
+console.log(isAlive)
