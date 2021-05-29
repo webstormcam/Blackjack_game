e5d8129a0e6774f5cac184ac0ecac252aeb89524
Cameron Chandler <webstormcam@gmail.com>
2021-05-29 08:11:31 -0400

    Made a slight adjustment to the conditional statement

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let firstCard = 10
 let secondCard = 7
-let sum = firstCard + secondCard +6
+let sum = firstCard + secondCard 
 
-if (sum<21){
+if (sum<=20){
     console.log('Do you want to draw a new card?🙂')
 } else if (sum===21){
     console.log("Woohoo! You've got Blackjack!🥳 ")
